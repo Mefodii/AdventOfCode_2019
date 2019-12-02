b569b3e751e785826d42da069a0ce7ed70678cf0
Mefodii <mefodii04@gmail.com>
2019-12-02 20:59:16 +0200

    Refactoring 3.0 (Removed generation of all file names for every day)

--- day_runner.py
+++ day_runner.py
@@ -1,5 +1,6 @@
-import paths
-from constants import PART_ONE, PART_TWO
+from paths import INPUT_FILES_PATH, OUTPUT_FILES_PATH
+from constants import PART_ONE, PART_TWO, FILE_TEMPLATES, DAY_TAG
+from constants import INPUT_TYPE, INPUT_SAMPLE_TYPE, OUTPUT_A_TYPE, OUTPUT_B_TYPE, OUTPUT_SAMPLE_TYPE
 from utils.File import read_file, write_file
 from days import day_1, day_2
 
@@ -10,33 +11,28 @@ DAYS = {
 
 
 def get_io_files(day, part, sample):
-    input_a_file = paths.get_file_for_day(paths.INPUT_TYPE, day)
-    input_sample_file = paths.get_file_for_day(paths.INPUT_SAMPLE_TYPE, day)
-    output_a_file = paths.get_file_for_day(paths.OUTPUT_TYPE_A, day)
-    output_b_file = paths.get_file_for_day(paths.OUTPUT_TYPE_B, day)
-    output_sample_file = paths.get_file_for_day(paths.OUTPUT_SAMPLE_TYPE, day)
-
-    input_file = input_sample_file
-    output_file = output_sample_file
-
-    if not sample:
-        input_file = input_a_file
+    if sample:
+        input_file = FILE_TEMPLATES[INPUT_SAMPLE_TYPE].replace(DAY_TAG, str(day))
+        output_file = FILE_TEMPLATES[OUTPUT_SAMPLE_TYPE].replace(DAY_TAG, str(day))
+    else:
+        input_file = FILE_TEMPLATES[INPUT_TYPE].replace(DAY_TAG, str(day))
         if part == PART_ONE:
-            output_file = output_a_file
-        elif part == PART_TWO:
-            output_file = output_b_file
+            output_file = FILE_TEMPLATES[OUTPUT_A_TYPE].replace(DAY_TAG, str(day))
+        else:
+            output_file = FILE_TEMPLATES[OUTPUT_B_TYPE].replace(DAY_TAG, str(day))
 
-    return [input_file, output_file]
+    input_file_path = "\\".join([INPUT_FILES_PATH, input_file])
+    output_file_path = "\\".join([OUTPUT_FILES_PATH, output_file])
+    return [input_file_path, output_file_path]
 
 
 def run(day, part, sample=False):
     input_file, output_file = get_io_files(day, part, sample)
 
+    output_data = ["None"]
     if part == PART_ONE:
         output_data = DAYS[day].run_a(read_file(input_file))
     elif part == PART_TWO:
         output_data = DAYS[day].run_b(read_file(input_file))
 
     write_file(output_file, output_data)
-
-
