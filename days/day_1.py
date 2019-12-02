1de249c986008ee47204115360a8824510c51614
Mefodii <mefodii04@gmail.com>
2019-12-02 12:57:06 +0200

    Day 1

--- days/day_1.py
+++ days/day_1.py
@@ -13,12 +13,27 @@ OUTPUT_B_FILE = paths.get_file_for_day(paths.OUTPUT_TYPE_B, DAY)
 OUTPUT_SAMPLE_FILE = paths.get_file_for_day(paths.OUTPUT_SAMPLE_TYPE, DAY)
 
 
+def calculate_fuel(mass):
+    return (mass // 3) - 2
+
+
+###############################################################################
 def run_a(input_data):
-    return ""
+    total_fuel_req = 0
+    for line in input_data:
+        total_fuel_req += calculate_fuel(int(line))
+    return [total_fuel_req]
 
 
 def run_b(input_data):
-    return ""
+    total_fuel_req = 0
+    for line in input_data:
+        mass = int(line)
+        while mass > 0:
+            mass = calculate_fuel(mass)
+            if mass > 0:
+                total_fuel_req += mass
+    return [total_fuel_req]
 
 
 def run(run_type=""):
