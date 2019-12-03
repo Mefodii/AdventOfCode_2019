212732f7ecc8357a1e76561d6e3f693f9dad4089
Mefodii <mefodii04@gmail.com>
2019-12-03 08:51:06 +0200

    Day 3 (Part 1)

--- days/day_3.py
+++ days/day_3.py
@@ -9,30 +9,13 @@ CROSS = "X"
 CENTRAL = "O"
 EMPTY = "."
 
-GRID_X, GRID_Y = 40000, 5000
-START_X, START_Y = GRID_X / 2, GRID_Y / 2
-
-
-def print_grid(grid):
-    lines = []
-    for column in range(len(grid[0]) - 1, -1, -1):
-        print_row = []
-        for row in range(len(grid)):
-            print_row.append(str(grid[row][column]))
-
-        lines.append("".join(print_row))
-
-    return lines
-
-
-def generate_grid(x, y):
-    grid = [[EMPTY for j in range(x)] for i in range(y)]
-    grid[START_X][START_Y] = CENTRAL
-    return grid
+START_X, START_Y = 0, 0
 
 
 def plot_cell(cell_value, wire_id):
-    if cell_value == CROSS or cell_value == CENTRAL or cell_value == str(wire_id):
+    if cell_value is None:
+        return str(wire_id)
+    elif cell_value == CROSS or cell_value == CENTRAL or cell_value == str(wire_id):
         pass
     elif not cell_value == EMPTY:
         return CROSS
@@ -43,7 +26,8 @@ def plot_cell(cell_value, wire_id):
 def plot_up(grid, distance, x, y, wire_id):
     for i in range(distance):
         y += 1
-        grid[x][y] = plot_cell(grid[x][y], wire_id)
+        coords = f"{x},{y}"
+        grid[coords] = plot_cell(grid.get(coords, None), wire_id)
 
     return [grid, x, y]
 
@@ -51,7 +35,8 @@ def plot_up(grid, distance, x, y, wire_id):
 def plot_down(grid, distance, x, y, wire_id):
     for i in range(distance):
         y -= 1
-        grid[x][y] = plot_cell(grid[x][y], wire_id)
+        coords = f"{x},{y}"
+        grid[coords] = plot_cell(grid.get(coords, None), wire_id)
 
     return [grid, x, y]
 
@@ -59,7 +44,8 @@ def plot_down(grid, distance, x, y, wire_id):
 def plot_left(grid, distance, x, y, wire_id):
     for i in range(distance):
         x -= 1
-        grid[x][y] = plot_cell(grid[x][y], wire_id)
+        coords = f"{x},{y}"
+        grid[coords] = plot_cell(grid.get(coords, None), wire_id)
 
     return [grid, x, y]
 
@@ -67,7 +53,8 @@ def plot_left(grid, distance, x, y, wire_id):
 def plot_right(grid, distance, x, y, wire_id):
     for i in range(distance):
         x += 1
-        grid[x][y] = plot_cell(grid[x][y], wire_id)
+        coords = f"{x},{y}"
+        grid[coords] = plot_cell(grid.get(coords, None), wire_id)
 
     return [grid, x, y]
 
@@ -85,13 +72,12 @@ def calculate_distance(x1, y1, x2, y2):
 
 
 def closest_cross(grid):
-    closest = len(grid) + len(grid[0])
-    for x in range(len(grid)):
-        for y in range(len(grid[0])):
-            if grid[x][y] == CROSS:
-                distance = calculate_distance(START_X, START_Y, x, y)
-                if distance < closest:
-                    closest = distance
+    closest = 9999999
+    for key, value in grid.items():
+        x, y = key.split(",")
+        if value == CROSS:
+            distance = calculate_distance(START_X, START_Y, int(x), int(y))
+            closest = min(distance, closest)
 
     return closest
 
@@ -106,37 +92,18 @@ def plot_wire(grid, commands, wire_id):
         grid, grid_x, grid_y = PLOTS[direction](grid, distance, grid_x, grid_y, wire_id)
 
 
-def calculate_grid_limits(commands):
-    l_x, h_x, l_y, h_y = 0, 0, 0, 0
-    for command in commands:
-        direction = command[0]
-        distance = int(command[1:])
-
-        if direction == UP:
-            h_x += distance
-        if direction == DOWN:
-            l_x += distance
-        if direction == LEFT:
-            l_y += distance
-        if direction == RIGHT:
-            h_y += distance
-
-    return [l_x, h_x, l_y, h_y]
-
-
-
 ###############################################################################
 def run_a(input_data):
-    # grid = generate_grid(30000, 30000)
+    grid = {
+        f"{START_X},{START_Y}": CENTRAL
+    }
 
     wire_id = 0
     for wire in input_data:
-        print(calculate_grid_limits(wire.split(",")))
         wire_id += 1
-        # plot_wire(grid, wire.split(","), wire_id)
+        plot_wire(grid, wire.split(","), wire_id)
 
-    # write_file("debug.txt", print_grid(grid))
-    # return [closest_cross(grid)]
+    return [closest_cross(grid)]
 
 
 def run_b(input_data):
