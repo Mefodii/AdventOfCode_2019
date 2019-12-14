ce968c9ca1d6d99c91861459fe36f0f49e658ab1
Mefodii <mefodii04@gmail.com>
2019-12-14 14:17:11 +0200

    Preparation

--- day_runner.py
+++ day_runner.py
@@ -3,7 +3,8 @@ from constants import PART_ONE, PART_TWO, FILE_TEMPLATES, DAY_TAG
 from constants import INPUT_TYPE, INPUT_SAMPLE_TYPE, OUTPUT_A_TYPE, OUTPUT_B_TYPE, OUTPUT_SAMPLE_TYPE
 from utils.File import read_file, write_file
 from days import day_1, day_2, day_3, day_4, day_5, day_6, day_7, day_8, day_9, day_10, \
-    day_11, day_12, day_13, day_14, day_15
+    day_11, day_12, day_13, day_14, day_15, day_16, day_17, day_18, day_19, day_20, \
+    day_21, day_22, day_23, day_24, day_25
 
 DAYS = {
     1: day_1,
@@ -21,6 +22,16 @@ DAYS = {
     13: day_13,
     14: day_14,
     15: day_15,
+    16: day_16,
+    17: day_17,
+    18: day_18,
+    19: day_19,
+    20: day_20,
+    21: day_21,
+    22: day_22,
+    23: day_23,
+    24: day_24,
+    25: day_25,
 }
 
 
